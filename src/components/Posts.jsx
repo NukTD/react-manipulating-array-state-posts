2300793ab90528151cd9ec97c421e624785723b4
TheYami <luffy90san@gmail.com>
2024-10-24 13:47:10 +0700

    feat: Add function to increase and decrease like count

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,28 +1,48 @@
+import { useState } from "react";
+import postData from "../data/postData";
+
 function Posts() {
+
+  const [postDatas,setPostDatas] = useState(postData)
+
+  const increaseLike = (likeCount) => {
+    const newData = [...postDatas]
+    newData[likeCount-1].likes += 1;
+    setPostDatas(newData);
+  }
+
+  const decreaseLike = (likeCount) => {
+    const newData = [...postDatas];
+    if (newData[likeCount - 1].likes > 0) {
+      newData[likeCount - 1].likes -= 1;
+    }
+    setPostDatas(newData);
+  };
+  
+
   return (
     <div class="app-wrapper">
       <h1 class="app-title">Posts</h1>
       <div class="post-list">
-        <div class="post-item">
-          <div class="post-header">
-            <h2>Post Title #1</h2>
-            <div class="post-social-media-stats">
-              <span class="stats-topic">Likes: </span>
-              <span class="post-likes">10</span>
+
+        {postDatas.map((data)=>(         
+          <div class="post-item" key={data.id}>
+            <div class="post-header">
+              <h2>{data.title}</h2>
+              <div class="post-social-media-stats">
+                <span class="stats-topic">Likes: </span>
+                <span class="post-likes">{data.likes}</span>
+              </div>
+            </div>
+            <p class="post-content">
+              {data.content}
+            </p>
+            <div class="post-actions">
+              <button class="like-button" onClick={()=>increaseLike(data.id)}>Like</button>
+              <button class="dislike-button" onClick={()=>decreaseLike(data.id)}>Dislike</button>
             </div>
           </div>
-          <p class="post-content">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
-            vel turpis vestibulum, aliquet ipsum vitae, auctor risus. Morbi
-            tincidunt, leo non molestie consectetur, elit libero faucibus
-            tellus, sed fringilla tortor libero sit amet odio. Maecenas sed ante
-            condimentum mauris euismod pellentesque eu eu justo...
-          </p>
-          <div class="post-actions">
-            <button class="like-button">Like</button>
-            <button class="dislike-button">Dislike</button>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
